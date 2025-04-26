6516749a6e07ebbf096480d44ade4966d3c15240
shruthirajaraman <88691726+shruthirajaraman@users.noreply.github.com>
2025-04-26 19:05:27 -0400

    Update meme-chip.sh

--- meme-chip.sh
+++ meme-chip.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-# Load MEME-suite module
+# Load MEME-suite, bedtools and samtools
 module load MEME-suite/5.4.1
 module load bedtools
+module load samtools
 
-# Input a list of bed file names and a list of output file names
+# Input a list of bed file names and a list of output file names (do not include .bed or .fa extensions)
 bed_file=($1)
 outputFile_names=($2)
 
-# Create a new directory for the resulting fasta files
-mkdir -p "bed2fasta_files"
+# Create a .fai file for the mouse genome (mm10.fa)
+samtools faidx "mm10.fa"
 
-# Map the chromosome coordinates in the bed file to the corresponding nucleotide regions in the mouse genome
+# Run MEME-chip
 for ((i=0; i<"${#bed_file[@]}"; i++)); do 
-    echo "Running: bed2fasta -o ${bed_file[$i]} mm10.fa ${outputFile_names[$i]}"
-    touch "${outputFile_names[$i]}"
-    bedtools getfasta -fi "mm10.fa" -bed ${bed_file[$i]} -s -fo ${outputFile_names[$i]}
-    mv ${outputFile_names[$i]} "bed2fasta_files"
 
-done
+    # Create output file and directory
+    touch "${outputFile_names[$i]}.fa"
+    new_dir="meme_${outputFile_names[$i]}"
+    mkdir -p $new_dir
 
-# Run MEME-chip on each of the fasta sequences
-cd "bed2fasta_files"
-for fasta_file in "${outputFile_names[@]}"; do
-    meme-chip $fasta_file
+    # Map the chromosome coordinates in the bed file to the corresponding nucleotide regions in the mouse genome
+    bedtools getfasta -fi "mm10.fa" -bed "${bed_file[$i]}.bed" -s -fo "${outputFile_names[$i]}.fa"
+    mv "${outputFile_names[$i]}.fa" $new_dir
+
+    # Run MEME-chip on the generated fasta file
+    cd $new_dir
+    meme-chip "${outputFile_names[$i]}.fa"
+    cd ..
 
 done
